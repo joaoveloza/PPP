e4a050292daecc6e0481b589f0ea056dfc25fe2c
joaoveloza <joaoveloza@gmail.com>
2025-03-17 20:41:31 +0000

    commit

---
+++ ch03/3.exercise.03.cpp
@@ -0,0 +1,19 @@
+//3.exercise.03.cpp
+//
+//Write a program that doesn't do anything, but declares a number of variables with legal and ilegal names (such as int double = 0;), so that you can see how the compiler reacts.
+
+#include <iostream>
+#include <string>
+
+int main() {
+// int double;
+// double delete;
+ char _wtf;
+ bool isfalse;
+ 
+ std:: string I_want$s {"Hola"};
+ std::cout << I_want$s << '\n';
+ int test test;
+ }
+
+
